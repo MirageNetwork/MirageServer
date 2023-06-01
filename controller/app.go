ceeb16a5a09611006241dbb48927d7309d85aa4a
chuanh <chuanh@opera.com>
2023-06-01 15:24:10 +0800

    修改get/set lastChange 为租户维度，并且恢复对所有租户配置和获取最新更新时间的操作

--- controller/app.go
+++ controller/app.go
@@ -8,7 +8,7 @@ import (
 	"io/fs"
 	"net"
 	"net/http"
-	"sort"
+	"strconv"
 	"sync"
 	"time"
 
@@ -606,68 +606,46 @@ func (h *Mirage) setLastStateChangeToNow() {
 
 	now := time.Now().UTC()
 
-	users, err := h.ListUsers()
+	orgs, err := h.ListOrgnaizations()
 	if err != nil {
 		log.Error().
 			Caller().
 			Err(err).
-			Msg("failed to fetch all users, failing to update last changed state.")
+			Msg("failed to fetch all organizations, failing to update last changed state.")
 	}
 
-	for _, user := range users {
+	for _, org := range orgs {
 		if h.lastStateChange == nil {
 			h.lastStateChange = xsync.NewMapOf[time.Time]()
 		}
-		h.lastStateChange.Store(user.StableID, now)
+		h.lastStateChange.Store("o:"+strconv.Itoa(int(org.ID)), now)
 	}
 }
 
 func (h *Mirage) setOrgLastStateChangeToNow(orgId ...int64) {
-	var err error
-	var users []User
 	now := time.Now().UTC()
-
-	if len(orgId) == 1 {
-		users, err = h.ListOrgUsers(orgId[0])
-	} else {
-		users, err = h.ListUsersInOrgs(orgId)
-	}
-	if err != nil {
-		log.Error().
-			Caller().
-			Err(err).
-			Msg("failed to fetch organization users, failing to update last changed state.")
-	}
-
-	for _, user := range users {
+	for _, id := range orgId {
 		if h.lastStateChange == nil {
 			h.lastStateChange = xsync.NewMapOf[time.Time]()
 		}
-		h.lastStateChange.Store(user.StableID, now)
+		h.lastStateChange.Store("o:"+strconv.Itoa(int(id)), now)
 	}
 }
 
-func (h *Mirage) getLastStateChange(users ...User) time.Time {
+func (h *Mirage) getLastStateChange() time.Time {
 	times := []time.Time{}
 
 	// getLastStateChange takes a list of users as a "filter", if no users
 	// are past, then use the entier list of users and look for the last update
-	if len(users) > 0 {
-		for _, user := range users {
-			if lastChange, ok := h.lastStateChange.Load(user.StableID); ok {
-				times = append(times, lastChange)
+	h.lastStateChange.Range(func(key string, value time.Time) bool {
+		if len(times) == 0 {
+			times = append(times, value)
+		} else {
+			if value.After(times[0]) {
+				times[0] = value
 			}
 		}
-	} else {
-		h.lastStateChange.Range(func(key string, value time.Time) bool {
-			times = append(times, value)
-
-			return true
-		})
-	}
-
-	sort.Slice(times, func(i, j int) bool {
-		return times[i].After(times[j])
+		return true
 	})
 
 	if len(times) == 0 {
@@ -678,31 +656,12 @@ func (h *Mirage) getLastStateChange(users ...User) time.Time {
 }
 
 func (h *Mirage) getOrgLastStateChange(orgId int64) time.Time {
-	times := []time.Time{}
-
-	// getLastStateChange takes a list of users as a "filter", if no users
-	// are past, then use the entier list of users and look for the last update
-	users, err := h.ListOrgUsers(orgId)
-	if err != nil {
-		log.Error().
-			Caller().
-			Err(err).
-			Msg("failed to fetch organization users, failing to get last changed state.")
-	}
-	for _, user := range users {
-		if lastChange, ok := h.lastStateChange.Load(user.StableID); ok {
-			times = append(times, lastChange)
-		}
-	}
-
-	if len(times) == 0 {
+	lastChange, ok := h.lastStateChange.Load("o:" + strconv.Itoa(int(orgId)))
+	if ok {
+		return lastChange
+	} else {
 		return time.Now().UTC()
 	}
-	sort.Slice(times, func(i, j int) bool {
-		return times[i].After(times[j])
-	})
-
-	return times[0]
 }
 
 func stdoutHandler(
